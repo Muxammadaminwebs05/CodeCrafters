ffc3d23963a8e7859b8325d95f27ca66ee0d9c73
Muhammadamin <150646677+Muxammadaminwebs05@users.noreply.github.com>
2024-02-07 22:19:07 -1100

    Update server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,7 +9,7 @@ const PORT: number = Number(process.env.PORT) || 5000;
 import errorMiddleware from "../middleware/errorHandler.js"; 
 import swRouter from "../utils/swagger.js";
 import * as path from 'path';
-app.use(express.json());
+app.use(express.json()); 
 app.use(fileUpload({
     limits: {
         fileSize: 50 * 1024 * 1024
